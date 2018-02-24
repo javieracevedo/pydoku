2ef9b3dc58ac0f61ab78b0489831da84cb31cc7d
javieracevedo <javier.acevedo233@gmail.com>
2018-02-24 04:29:34 -0400

    Just updates...don't wanna write commit messages....:P

--- pydoku.py
+++ pydoku.py
@@ -1,13 +1,27 @@
 import pygame
 import libs.text_input as text_input
-#import libs.board as board
 import sudoku_board as board
 from pydoku_sprites import *
 from number_sprite_gen import *
 
 
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+GREEN = (0,255,0)
+RED = (255,0,0)
+
+
+
+
+pygame.font.init()
+winner_font = pygame.font.SysFont("monospace", 30)
+winner_font.set_bold(True)
+restart_font = pygame.font.SysFont("monospace", 15)
+restart_font.set_bold(True)
+
 # Window and Grid properties
-DSURFACE = pygame.display.set_mode((640, 640))
+window_dimension = (640,640)
+DSURFACE = pygame.display.set_mode(window_dimension)
 sudoku_board_dimensions = (9,9)
 background_color = (255,255,255)
 grid_lines_color = (0,0,0)
@@ -26,26 +40,132 @@ sudoku_board.highlight_color_alpha = 100
 textinput = text_input.TextInput()
 
 
+def draw_win():
+  # Shows the winner on the screen
+  overlay = pygame.Surface(DSURFACE.get_size())
+  overlay.set_alpha(200)
+  overlay.fill(BLACK)
+  DSURFACE.blit(overlay, (0, 0))
+  win_text = winner_font.render("YOU WIN", 1, WHITE)
+  restart_text = restart_font.render("Press (Space) to restart", 1, RED)
+
+  DSURFACE.blit(win_text, ((window_dimension[0] * 0.5) - win_text.get_width() * 0.5, (window_dimension[1] * 0.5 ) - win_text.get_height() * 0.5))
+  DSURFACE.blit(restart_text, ((window_dimension[0] * 0.5) - restart_text.get_width() * 0.5, (window_dimension[1]) * 0.6 ))
+  #pygame.display.flip()
+
+
 def select_board_cell(mouse_pos):
   sudoku_board.select_cell(mouse_pos)
 
 
 def populate_board():
-  sudoku_board_file = open("boards/sudokuboards.txt")
-  sudoku_board_file = sudoku_board_file.readlines()
-  for i in range(len(sudoku_board_file)):
-    for j in range(len(sudoku_board_file[i]) - 1):
-      #print "{}, {}".format(i,j)
-      cell = (i,j)
-      #sudoku_board.board[i][j] = sudoku_board_file[i][j]
-      print "populate: {}".format(cell)
-      if sudoku_board_file[i][j] != "0":
-        number = NumberSpriteGen.factory(int(sudoku_board_file[i][j]))
-        sudoku_board.insert_sprite(number, cell, str(i))
+  """
+    Reads a file with a sudoku board and inserts every value in the sudoku_board
+    Note: For now, assumes its a 9x9 board, always
+  """
+
+  print "pydoku:populate_board -- Populating Board"
+
+  lines = []
+  
+  sudoku_board_path = "boards/sudoku_board_2.txt"
+  # Read sudokuboards file and store the lines in a list
+  with open(sudoku_board_path) as f_in:
+    for line in nonblank_lines(f_in):
+      lines.append(line)
+
+  # Iterate over lines list and insert the corresponding values in the board
+  for x in range(len(lines)):
+    for y in range(len(lines[x])):
+      cell = (x,y)
+      if lines[x][y] != "0":
+        number = NumberSpriteGen.factory(int(lines[x][y]))
+        sudoku_board.insert_sprite(number,cell, int(lines[x][y]))        
+  
+# Utils
+def nonblank_lines(f):
+  for l in f:
+    line = l.rstrip()
+    if line:
+      yield line
+
+# check if a value is repeated more than on
+# on a list or a string
+def repeatedTwice(l, value, ignore):
+  # Counter to keep track of how much a value is repeated
+  times_repeated = 0
+  for item in l:
+    if item == value and item != ignore:
+      times_repeated += 1
+  
+  if times_repeated > 1:
+    return True
+  else:
+    return False
 
 
+def isBoardFull(board):
+  for arr in board:
+    if "0" in arr:
+      return False
+  return True
+
+
+def sudoku_check(board):
+  # First: check if the board is full (All cells have values)
+  # Starting from the top, left to right
+  # Grab the first cell value, and check vertically and horizontally if there is any other cell with the same value
+  # If the stated above is true, return false(not win)
+  # If the stated above is false, return true
+
+
+  # Iterate over board
+  # get row/column of current cell
+  # iterate over row/colum
+  # check if any of the row/colum cell's value is equal to current cell value
+
+  row, col = ("", "")
+
+
+
+  board_len = len(board)
+  
+  
+  board_full = isBoardFull(board)
+
+  # If board is full, check if there are any errors
+  if board_full:
+    # TODO: Im not particularly proud of this solution, I intend to fix this uglyness
+    # on the next version of my Board library.
+    for i in range(board_len):
+      for j in range(board_len):
+        current_cell_value = board[i][j]
+        row += str(board[i][j])
+        col += str(board[j][i])
+      
+      for c in row:
+        if repeatedTwice(row, c, "0"):
+          print "Uh oh! : {} repeated more than twice in row {}".format(c, row)
+          return False
+
+      for k in col:
+        if repeatedTwice(col, k, "0"):
+          print "Uh oh! : {} repeated more than twice in col {}".format(k, col)
+          return False
+      
+      row = ""
+      col = ""
+    
+
+    return True
+  
 # Read a sudoku board from text and insert it on the sudoku board
 populate_board()
+
+
+#sudoku_check(sudoku_board.board)
+
+
 
 
 # Grid events registrations
@@ -53,6 +173,7 @@ sudoku_board.register_event(select_board_cell, "on_cell_lmb_down")
 
 running = True
 while running:
+
 
   events = pygame.event.get()
 
@@ -78,5 +199,9 @@ while running:
         sudoku_board.insert_sprite(number, sudoku_board.current_cell_selected, str(i))
 
     textinput.clear_text()
+
+  check_win = sudoku_check(sudoku_board.board)
+  if check_win:
+    draw_win()
 
   pygame.display.update()
--- sudoku_board.py
+++ sudoku_board.py
@@ -1,25 +1,30 @@
 from libs.board import *
+import math
 
 
 class SudokuBoard(Board):
   def __init__(self, display_surface, board_dimension, background_color, lines_color):
+    self.board_dimension = board_dimension
     super(SudokuBoard, self).__init__(display_surface, board_dimension, background_color, lines_color)
 
   def __draw_grid(self, color, lw):
     ### Draws a 3x3 grid on the DSURFACE
     # Draw vertical lines
     cell_width = self.WIDTH/self.BOARD_W
-    for width in range(cell_width, self.WIDTH, cell_width):
-      if width % 3 == 0:
-        lines_width = lw * 5
+    #x_dimension = self.board_dimension[0]
+    #y_dimension = self.board_dimension[1]
+    for width in range(0, self.WIDTH, cell_width):
+      if width % 3 == 0: # For now just asume that the board should be divided in three
+        lines_width = lw * 5 # Make the third and sixth line thicker
+
       else:
         lines_width = lw
       pygame.draw.line(self.DSURFACE, color, [width, 0], [width, self.HEIGHT], lines_width)
 
     # Draw horizontal lines
     cell_height = self.HEIGHT/self.BOARD_H
-    for height in range(cell_height, self.HEIGHT, cell_height):
-      if height % 3 == 0:
+    for height in range(0, self.HEIGHT, cell_height):
+      if height % 3 == 0: # For now just asume that the board should be divided in three
         lines_width = lw * 5
       else:
         lines_width = lw
@@ -30,7 +35,6 @@ class SudokuBoard(Board):
     self.__draw_grid(self.lines_color, 1)
     self.event_handling(events)
 
-
     ### Grid Update ###
 
     # Highlight the current selected cell, if any
@@ -39,6 +43,3 @@ class SudokuBoard(Board):
 
     # Update sprites on grid
     self.sprite_group.draw(self.DSURFACE)
-
-
-    pygame.display.update()
