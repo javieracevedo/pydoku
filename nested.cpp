6313726940d837247d642c0c254c53552342c08b
Javier Acevedo <javier.acevedo233@gmail.com>
2018-06-29 11:00:39 -0400

    nested.cpp

---
+++ nested.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main(){
+  int g = 10;
+  for (int i=0; i<10; i++){
+    for (int j=0; j<g; j++){
+      cout << "*";
+    }
+  cout << endl;
+  g--;
+  }
+}
